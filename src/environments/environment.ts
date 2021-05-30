3261f26f4b1cada9c318fe656ec1350bb84392a6
Sanjay Mishra <mishra.sanjay9425@gmail.com>
2021-05-31 01:50:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAlyYoYAAsnExMYd8dcl2QTHNeC6SkWE0I',
-    authDomain: 'friendlychat-c6553.firebaseapp.com',
-    databaseURL: 'https://friendlychat-c6553.firebaseio.com',
-    projectId: 'friendlychat-c6553',
-    storageBucket: 'friendlychat-c6553.appspot.com',
-    messagingSenderId: '664696296928',
-    appId: '1:664696296928:web:9ae37dbf396d3ed6',
+    //add firebase credentials
   },
 };
 
